c87b224f51121efb4e1f7ba1b8c40834affbd5af
Misha Aizatulin <avatar@hot.ee>
2011-08-25 23:14:49 +0100

    ocamlyices is now an external module

--- deps/openssl-1.0.0d/crypto/opensslconf.h
+++ deps/openssl-1.0.0d/crypto/opensslconf.h
@@ -124,14 +124,14 @@
 
 #if defined(HEADER_BN_H) && !defined(CONFIG_HEADER_BN_H)
 #define CONFIG_HEADER_BN_H
-#define BN_LLONG
+#undef BN_LLONG
 
 /* Should we define BN_DIV2W here? */
 
 /* Only one for the following should be defined */
-#undef SIXTY_FOUR_BIT_LONG
+#define SIXTY_FOUR_BIT_LONG
 #undef SIXTY_FOUR_BIT
-#define THIRTY_TWO_BIT
+#undef THIRTY_TWO_BIT
 #endif
 
 #if defined(HEADER_RC4_LOCL_H) && !defined(CONFIG_HEADER_RC4_LOCL_H)
