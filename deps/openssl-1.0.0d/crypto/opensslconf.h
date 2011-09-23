3cfea76e3247783bacef39807e3fdd64d4d82fd3
U-tango\avatar <avatar@tango.(none)>
2011-09-23 05:15:41 +0100

    split makefiles in rpc-enc

--- deps/openssl-1.0.0d/crypto/opensslconf.h
+++ deps/openssl-1.0.0d/crypto/opensslconf.h
@@ -124,14 +124,14 @@
 
 #if defined(HEADER_BN_H) && !defined(CONFIG_HEADER_BN_H)
 #define CONFIG_HEADER_BN_H
-#undef BN_LLONG
+#define BN_LLONG
 
 /* Should we define BN_DIV2W here? */
 
 /* Only one for the following should be defined */
-#define SIXTY_FOUR_BIT_LONG
+#undef SIXTY_FOUR_BIT_LONG
 #undef SIXTY_FOUR_BIT
-#undef THIRTY_TWO_BIT
+#define THIRTY_TWO_BIT
 #endif
 
 #if defined(HEADER_RC4_LOCL_H) && !defined(CONFIG_HEADER_RC4_LOCL_H)
